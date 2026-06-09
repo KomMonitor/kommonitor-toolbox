d4158f8b6c7dc75cd2647a397e65c8b813d69424
Jan Schulte <j.schulte@52north.org>
2026-06-09 12:34:37 +0200

    restructure files

--- projects/kommonitor-toolbox/src/lib/kommonitor-toolbox.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'lib-kommonitor-toolbox',
-  imports: [],
-  template: ` <p>kommonitor-toolbox works!</p> `,
-  styles: ``,
-})
-export class KommonitorToolbox {}
--- projects/kommonitor-toolbox/src/public-api.ts
+++ projects/kommonitor-toolbox/src/public-api.ts
@@ -2,11 +2,10 @@
  * Public API Surface of kommonitor-toolbox
  */
 
-export * from './lib/bar-chart/bar-chart.component';
-export * from './lib/bar-chart/bar-chart.types';
-export * from './lib/kommonitor-toolbox';
-export * from './lib/map/map.component';
-export * from './lib/map/map.types';
+export * from './lib/components/bar-chart/bar-chart.component';
+export * from './lib/components/bar-chart/bar-chart.types';
+export * from './lib/components/map/map.component';
+export * from './lib/components/map/map.types';
+export * from './lib/components/timeseries-chart/timeseries-chart.component';
+export * from './lib/components/timeseries-chart/timeseries-chart.types';
 export * from './lib/services';
-export * from './lib/timeseries-chart/timeseries-chart.component';
-export * from './lib/timeseries-chart/timeseries-chart.types';
